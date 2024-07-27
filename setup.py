20c167b43532bee3a2d2c7f056941986638b2ea0
OUAHABI Benhenni <40049913+Greyma@users.noreply.github.com>
2024-07-27 14:13:51 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,35 +1,18 @@
-"""
-The build/compilations setup
-
->> pip install -r requirements.txt
->> python setup.py install
-"""
-import pip
 import logging
-import pkg_resources
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+from setuptools import setup, find_packages
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
 def _parse_requirements(file_path):
-    pip_ver = pkg_resources.get_distribution('pip').version
-    pip_version = list(map(int, pip_ver.split('.')[:2]))
-    if pip_version >= [6, 0]:
-        raw = pip.req.parse_requirements(file_path,
-                                         session=pip.download.PipSession())
-    else:
-        raw = pip.req.parse_requirements(file_path)
-    return [str(i.req) for i in raw]
+    try:
+        requirements = parse_requirements(file_path, session=PipSession())
+        return [str(requirement.requirement) for requirement in requirements]
+    except Exception as e:
+        logging.warning(f"Failed to load requirements file, using default ones. Error: {e}")
+        return []
 
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-try:
-    install_reqs = _parse_requirements("requirements.txt")
-except Exception:
-    logging.warning('Fail load requirements file, so using default ones.')
-    install_reqs = []
+# Parse requirements
+install_reqs = _parse_requirements("C:\\Users\\Cherif\\Desktop\\GitHub For Project iA\\Mask-RCNN-2024\\requirements.txt")
 
 setup(
     name='mask-rcnn',
@@ -39,10 +22,10 @@ setup(
     author_email='waleed.abdulla@gmail.com',
     license='MIT',
     description='Mask R-CNN for object detection and instance segmentation',
-    packages=["mrcnn"],
+    packages=find_packages(exclude=["tests", "docs"]),
     install_requires=install_reqs,
     include_package_data=True,
-    python_requires='>=3.4',
+    python_requires='>=3.6',
     long_description="""This is an implementation of Mask R-CNN on Python 3, Keras, and TensorFlow. 
 The model generates bounding boxes and segmentation masks for each instance of an object in the image. 
 It's based on Feature Pyramid Network (FPN) and a ResNet101 backbone.""",
@@ -60,9 +43,10 @@ It's based on Feature Pyramid Network (FPN) and a ResNet101 backbone.""",
         "Topic :: Scientific/Engineering :: Image Recognition",
         "Topic :: Scientific/Engineering :: Visualization",
         "Topic :: Scientific/Engineering :: Image Segmentation",
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords="image instance segmentation object detection mask rcnn r-cnn tensorflow keras",
 )
